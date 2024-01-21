d3097c9e037629f7cf85dd7b6ad1572fc6e4d2ad
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-01-21 08:36:48 -0500

    prueba del icono

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st.set_page_config(page_title="Flujo de caja", page_icon=":bar_chart:", layout="wide")
