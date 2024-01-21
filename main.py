430ea01ffb2443ad42b5b3b6baf8184a3d780f87
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-01-21 08:48:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import streamlit as st
 
 # Add custom CSS to hide the GitHub icon
 hide_github_icon = """
+<style>
 #MainMenu {
   visibility: hidden;
 }
+footer {
+  visibility: hidden;
+}
+</style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
